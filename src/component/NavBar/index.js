4fdc6c7f0235764a7ce8ec445c602195b3fd94a6
Vijaya03 <36135722+Vijaya03@users.noreply.github.com>
2022-04-13 13:13:13 +0530

    issue second fixed

--- src/component/NavBar/index.js
+++ src/component/NavBar/index.js
@@ -10,17 +10,17 @@ const NavBar = () =>{
       }
     return (
             <nav>
-                <logo>
+                <span>
                     <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g id="Group 2">
+                        <g id="Group2">
                         <circle id="Oval" cx="24" cy="24" r="24" fill="white"/>
                         <path id="Path" fill-rule="evenodd" clip-rule="evenodd" d="M24 0C24 0 24 24 0 24C23.5776 24.1714 24 48 24 48C24 48 24 24 48 24C24 24 24 0 24 0Z" fill="#0B0D17"/>
                         </g>
                     </svg>
-                </logo>
+                </span>
                 <span className="hamburger" area-controls="navi" area-expanded={toggle} onClick={()=>setToggle("true")}>
                     <svg width="24" height="21" viewBox="0 0 24 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g id="Group">
+                        <g id="Group1">
                         <rect id="Rectangle" width="24" height="3" fill="#D0D6F9"/>
                         <rect id="Rectangle Copy" y="9" width="24" height="3" fill="#D0D6F9"/>
                         <rect id="Rectangle Copy 2" y="18" width="24" height="3" fill="#D0D6F9"/>
@@ -31,7 +31,7 @@ const NavBar = () =>{
                 <hr className="dash"></hr>
                 <span className="cross" onClick={()=>setToggle("false")}>
                     <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g id="Group">
+                        <g id="Group3">
                         <rect id="Rectangle Copy" x="2.5752" y="0.954102" width="24" height="3" transform="rotate(45 2.5752 0.954102)" fill="#D0D6F9"/>
                         <rect id="Rectangle Copy 3" x="0.454102" y="17.9246" width="24" height="3" transform="rotate(-45 0.454102 17.9246)" fill="#D0D6F9"/>
                         </g>
